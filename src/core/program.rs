b4984a3f49a191b4f5aa783a2d25089a4a6a1f4e
Soham Ghugare <soham.ghugare@gmail.com>
2026-02-25 05:28:34 +0530

    feat: add to_bytes impl for program struct

--- src/core/program.rs
+++ src/core/program.rs
@@ -1,8 +1,72 @@
-use crate::core::{constant::ConstEntry, footer::Footer, header::Header, instruction::Instruction};
+use crate::{
+    core::{
+        constant::{ConstEntry, QISA_CONST_ENTRY_SIZE},
+        footer::{Footer, QISA_FOOTER_SIZE},
+        header::{Header, QISA_HEADER_SIZE},
+        instruction::Instruction,
+    },
+    utils::fnv1a_64,
+};
 
 pub struct Program {
     pub header: Header,
     pub constants: Vec<ConstEntry>,
     pub instructions: Vec<Instruction>,
     pub footer: Footer,
+}
+
+impl Program {
+    pub fn to_bytes(&self) -> Vec<u8> {
+        let mut buffer = Vec::new();
+
+        // Placeholder for building header later on
+        buffer.resize(QISA_HEADER_SIZE, 0);
+
+        // ---------------------------
+        // Constant Pool
+        // ---------------------------
+        let const_pool_offset = QISA_HEADER_SIZE as u64;
+
+        for c in &self.constants {
+            buffer.extend_from_slice(&c.serialize());
+        }
+
+        let const_pool_size = (self.constants.len() * QISA_CONST_ENTRY_SIZE) as u64;
+
+        // ---------------------------
+        // Instruction Stream
+        // ---------------------------
+        let instruction_stream_offset = buffer.len() as u64;
+
+        for i in &self.instructions {
+            buffer.extend_from_slice(&i.serialize());
+        }
+
+        let instruction_stream_size = (buffer.len() as u64) - instruction_stream_offset;
+
+        // Footer placeholder
+        let footer_offset = buffer.len();
+        buffer.resize(footer_offset + QISA_FOOTER_SIZE, 0);
+
+        // ---------------------------
+        // Build Header
+        // ---------------------------
+        let header = self.header.build(
+            self.instructions.len() as u64,
+            const_pool_offset,
+            instruction_stream_offset,
+            const_pool_size,
+            instruction_stream_size,
+        );
+
+        buffer[0..QISA_HEADER_SIZE].copy_from_slice(&header.serialize());
+
+        // ---------------------------
+        // Program Checksum
+        // ---------------------------
+        let program_checksum = fnv1a_64(&buffer[..footer_offset]);
+        buffer[footer_offset..footer_offset + 8].copy_from_slice(&program_checksum.to_le_bytes());
+
+        buffer
+    }
 }
